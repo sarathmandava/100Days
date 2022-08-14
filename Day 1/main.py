e871e583c3a409552d1a26447dca11b7ed0ce4cc
Sarath Mandava <sarath.mandava7@gmail.com>
2022-08-14 01:51:06 +0000

    Day 1

---
+++ Day 1/main.py
@@ -0,0 +1,7 @@
+#Day 1 Main
+print("band name generator - ta daaa!")
+print("Your band name could be "+input("What is your Name?\n")+" "+input("What is your Pet Name?\n"))
+
+## The output checks Out! YAY! 
+
+# Sarath - 08/13 9/49 PM
---
+++ Day 1/test.py
@@ -0,0 +1,20 @@
+print("Hey World!") #Printing Lesson
+
+#Debugging
+print("Day 1 - Python Print Function \nThe function is declared like this: \nprint(\"what to print\")")
+
+#The solution have three print statements and single quote around print inside text. I used new line and escape charecters to get double quotes. Keeping it real? :D
+
+#More printing lol
+
+print("Hello"+" "+"Sarath")
+
+#Indentation Error code should always start at the line beginning
+# print("Hello"+" "+"Sarath")
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the \"+\" sign.")
+print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n."))
+
+
+
