00c22f25afb31e367047f2241b58c9ca8a66097a
Sarath Mandava <sarath.mandava7@gmail.com>
2022-08-15 12:29:28 +0000

    int to float

--- Day 2/test.py
+++ Day 2/test.py
@@ -1,14 +1,14 @@
 print("Tip Calculator and Splitter!")
 
 def plus_tip(check_total,tip_percent):
-  return(check_total+(check_total*(tip_percent/100)))
+  return(round(check_total+(check_total*(tip_percent/100)),2))
 
 def splitter(parts):
   return(plus_tip(check_total,tip_percent)/parts)
 
-check_total = int(input("What is the Total?\t"))
-tip_percent = int(input("What percent do you want to Tip?\t"))
-parts = int(input("How many ways would you like to split?\t"))
+check_total = float(input("What is the Total?\t"))
+tip_percent = float(input("What percent do you want to Tip?\t"))
+parts = float(input("How many ways would you like to split?\t"))
 print("Total + Tip - ")
 print("$ "+str(plus_tip(check_total,tip_percent)))
 print("Individual Total - ")
