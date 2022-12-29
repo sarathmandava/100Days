2a81e227554894cbcc7ec7583d995310285d1d38
Sarath Mandava <mandava@Saraths-MacBook-Pro.local>
2022-12-29 13:46:59 -0600

    ex1-6

---
+++ LPTHW/ex2.py
@@ -0,0 +1,10 @@
+# The book says to type it out so let's do it!
+# A comment, this is so because you can read the program later
+# Anything after the # will be ignored by python
+
+print("I could have written program like this!") # This will be ignored 
+
+# you can also use # to "disable" the program
+# print("This won't print")
+
+print("This will print")
---
+++ LPTHW/ex5.py
@@ -0,0 +1,24 @@
+# Exercise 5, LFG
+
+my_name = "Sarath Mandava"
+my_age = 28 # ha ha 
+my_height = 70 #fake height in inches
+my_weight = 180 #lbs
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+## This is like shell {} flower braces are used to call the varible
+## the print statement starts with f, f stands for format
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"Actually that is heavy, get it down sir!")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# This line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} & {my_weight} I get {total}.")
+
