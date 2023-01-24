98e4119f532cc8a298df13e748bb2c8dc7e056e4
Sarath Mandava <mandava@Saraths-MacBook-Pro.local>
2023-01-23 20:32:25 -0600

    assorted cookies

---
+++ LPTHW/ex10.py
@@ -0,0 +1,17 @@
+# New day New day to code, LFG
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split \non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
